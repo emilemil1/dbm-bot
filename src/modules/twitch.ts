6a27e3e8fe428ff7ae1d730b042139d1a237d154
Emil <eminor992@gmail.com>
2020-05-16 23:46:18 +0200

    fix

--- src/modules/twitch.ts
+++ src/modules/twitch.ts
@@ -314,7 +314,7 @@ class Twitch implements CommandModule, WebhookModule {
             },
             body: JSON.stringify(
                 {
-                    "hub.callback": `${url}${port || ""}"/webhook/twitch`,
+                    "hub.callback": `${url}":"${port}"/webhook/twitch`,
                     "hub.mode": subscribe ? "subscribe" : "unsubscribe",
                     "hub.topic": "https://api.twitch.tv/helix/streams?user_id=" + channelInfo.id,
                     "hub.lease_seconds": "864000"
